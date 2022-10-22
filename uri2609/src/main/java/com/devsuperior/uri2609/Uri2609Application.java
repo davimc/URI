7a6ec46a14d41aa90c5148276cabcecfd4a3ae57
davimc <davimatosc@hotmail.com>
2022-10-21 23:31:13 -0300

    uri 2609

--- uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2609;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2609.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2609Application implements CommandLineRunner {
@@ -19,6 +24,15 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		List<CategorySumProjection> list = repository.search1();
+		List<CategorySumDTO> result1 = list.stream().map(CategorySumDTO::new).collect(Collectors.toList());
 
+		for (CategorySumDTO x : result1)
+			System.out.println(x);
+
+		List<CategorySumDTO> result2 = repository.search2();
+
+			for (CategorySumDTO x : result2)
+				System.out.println(x);
 	}
 }
--- uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,24 @@
 package com.devsuperior.uri2609.repositories;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2609.entities.Category;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    @Query(nativeQuery = true, value = "SELECT categories.name, SUM(products.amount) " +
+            "FROM products " +
+            "INNER JOIN categories ON categories.id = products.id_categories " +
+            "GROUP BY categories.name")
+    public List<CategorySumProjection> search1();
+
+    @Query("SELECT new  com.devsuperior.uri2609.dto.CategorySumDTO(obj.category.name, SUM(obj.amount)) " +
+            "FROM Product obj " +
+            "GROUP BY obj.category.name")
+    public List<CategorySumDTO> search2();
 }
