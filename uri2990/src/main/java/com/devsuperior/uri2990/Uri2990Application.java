48fcbab436c87eb3705293b3972190f0e8f1fe25
davimc <davimatosc@hotmail.com>
2022-10-23 10:18:34 -0300

    uri 2990 (just native query)

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2990;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2990Application implements CommandLineRunner {
@@ -19,6 +24,10 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		List<EmpregadoDeptProjection> list = repository.search1();
+		List<EmpregadoDeptDTO> result1 = list.stream().map(EmpregadoDeptDTO::new).collect(Collectors.toList());
+
+		for (EmpregadoDeptDTO x : result1)
+			System.out.println(x);
 	}
 }
--- uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,22 @@
 package com.devsuperior.uri2990.repositories;
 
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2990.entities.Empregado;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
 
+    @Query(nativeQuery = true, value = "SELECT empregados.cpf, empregados.enome, departamentos.dnome " +
+            "FROM empregados " +
+            "INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero " +
+            "WHERE empregados.cpf NOT IN ( " +
+            "   SELECT empregados.cpf  " +
+            "   FROM empregados " +
+            "   INNER JOIN trabalha ON trabalha.cpf_emp = empregados.cpf " +
+            ") ")
+    public List<EmpregadoDeptProjection> search1();
 }
